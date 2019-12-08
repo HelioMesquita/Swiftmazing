b7b946893a935c46fd8f3c6554bf47b20f0674ad
Helio Mesquita <helio.mesquitaios@gmail.com>
2019-12-08 20:40:45 -0300

    added project

---
+++ Swiftmazing/Swiftmazing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swiftmazing
+//
+//  Created by Hélio Mesquita on 08/12/19.
+//  Copyright © 2019 Hélio Mesquita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
